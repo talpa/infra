00d4dcc189f5280d766ce375b3882279ae0839bc
mrbar2000 <mrbar2000@56d1e7d9-9621-0410-b556-b1a0954dd335>
2009-01-01 00:09:10 +0000

    [+] Criada uma unit para constantes relacionados a persistencia; [+] Criada quase todas as classes da persistência. Implementação incompleta; [+] Criada lista-map para armazenar parametros do SQLCommand; [+] Alterada configuração dos testes para compilar dcus e exes no diretório correto;

---
+++ src/Core/Persistence/InfraPersistenceConsts.pas
@@ -0,0 +1,18 @@
+unit InfraPersistenceConsts;
+
+interface
+
+const
+  // Chaves do Configuration
+  cCONFIGKEY_MAXCONNECTIONS = 'Pool.MaxConnections';
+  cCONFIGKEY_CONNECTIONTIME = 'Pool.TimeExpirationConnection';
+
+  // Valores padrões para items do configuration
+  cGlobalMaxConnections = 30;
+
+  // Erros da persistência
+  cErrorConfigurationNotDefined = 'Configuration nao foi alimentado';
+
+implementation
+
+end.
