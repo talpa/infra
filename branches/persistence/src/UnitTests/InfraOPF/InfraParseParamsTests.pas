457a7c4ec6f770e5cb10622082212524beedc7d6
marcosg.duarte <marcosg.duarte@56d1e7d9-9621-0410-b556-b1a0954dd335>
2009-01-04 21:49:18 +0000

    NEW: Criado teste unitário (muito simples) para o ParamParser

---
+++ branches/persistence/src/UnitTests/InfraOPF/InfraParseParamsTests.pas
@@ -0,0 +1,109 @@
+unit InfraParseParamsTests;
+
+interface
+
+uses
+  SysUtils,
+  Classes,
+  InfraPersistence,
+  InfraPersistenceIntf,
+  TestFramework;
+
+type
+  TTestParseParams = class(TTestCase)
+  private
+  published
+    procedure TestParserSelect;
+    procedure TestParserInsert;
+    procedure TestParserUpdate;
+    procedure TestParserDelete;
+
+    procedure TestParserParamsRepeated;
+  end;
+
+implementation
+
+{ TTestParseParams }
+
+procedure TTestParseParams.TestParserInsert;
+const
+  cSql = 'insert into tabela (codigo, nome) values (:codigo, :nome)';
+var
+  vParser: IParseParams;
+  vParams: TStrings;
+begin
+  vParser := TParseParams.Create;
+  vParser.Parse(cSql);
+  vParams := vParser.GetParams;
+  CheckEquals(2, vParams.Count);
+  CheckEqualsString('codigo', vParams[0]);
+  CheckEqualsString('nome', vParams[1]);
+end;
+
+procedure TTestParseParams.TestParserSelect;
+const
+  cSql = 'select * from tabela where codigo = :codigo and nome = :nome';
+var
+  vParser: IParseParams;
+  vParams: TStrings;
+begin
+  vParser := TParseParams.Create;
+  vParser.Parse(cSql);
+  vParams := vParser.GetParams;
+  CheckEquals(2, vParams.Count);
+  CheckEqualsString('codigo', vParams[0]);
+  CheckEqualsString('nome', vParams[1]);
+end;
+
+procedure TTestParseParams.TestParserUpdate;
+const
+  cSql = 'update tabela set codigo = :codigo, nome = :nome where id = :id';
+var
+  vParser: IParseParams;
+  vParams: TStrings;
+begin
+  vParser := TParseParams.Create;
+  vParser.Parse(cSql);
+  vParams := vParser.GetParams;
+  CheckEquals(3, vParams.Count);
+  CheckEqualsString('codigo', vParams[0]);
+  CheckEqualsString('nome', vParams[1]);
+  CheckEqualsString('id', vParams[2]);
+end;
+
+procedure TTestParseParams.TestParserDelete;
+const
+  cSql = 'delete from tabela where codigo = :codigo and nome = :nome and id = :id';
+var
+  vParser: IParseParams;
+  vParams: TStrings;
+begin
+  vParser := TParseParams.Create;
+  vParser.Parse(cSql);
+  vParams := vParser.GetParams;
+  CheckEquals(3, vParams.Count);
+  CheckEqualsString('codigo', vParams[0]);
+  CheckEqualsString('nome', vParams[1]);
+  CheckEqualsString('id', vParams[2]);
+end;
+
+procedure TTestParseParams.TestParserParamsRepeated;
+const
+  cSql = 'update tabela set id = :id, codigo = :codigo, nome = :nome where id = :id';
+var
+  vParser: IParseParams;
+  vParams: TStrings;
+begin
+  vParser := TParseParams.Create;
+  vParser.Parse(cSql);
+  vParams := vParser.GetParams;
+  CheckEquals(4, vParams.Count);
+  CheckEqualsString('id', vParams[0]);
+  CheckEqualsString('codigo', vParams[1]);
+  CheckEqualsString('nome', vParams[2]);
+  CheckEqualsString('id', vParams[3]);
+end;
+
+initialization
+  TestFramework.RegisterTest(TTestParseParams.Suite);
+end.
