d850222e6928de6ad0676098647840e0484deeab
mrbar2000 <mrbar2000@56d1e7d9-9621-0410-b556-b1a0954dd335>
2009-01-04 17:13:40 +0000

    [+] Criado TParseParams para poder extrair quais os parâmetros da sql na ordem deles;

--- branches/persistence/src/UnitTests/InfraOPF/PersistenceTests.pas
+++ branches/persistence/src/UnitTests/InfraOPF/PersistenceTests.pas
@@ -17,17 +17,21 @@ type
     procedure TearDown; override;
   published
     procedure TestLoadObjectByOID;
+    procedure TestParse;
   end;
 
 implementation
 
 uses
+  Dialogs,
   SysUtils, 
   Forms, 
   Math,
+  Classes,
   InfraValueType, 
   InfraValueTypeIntf,
   PersistenceModel, 
+  InfraPersistence,
   InfraPersistenceConsts;
 
 { THibernateTests }
@@ -35,20 +39,18 @@ uses
 procedure TPersistenceTests.SetUp;
 begin
   inherited;
-  FConfiguration := PersistenceService.Configuration;
-  
   // Aqui é definido no Configuration algumas propriedades, para que o
-  // InfraPersistence saiba algumas coisas necessárias para configurar o 
+  // InfraPersistence saiba algumas coisas necessárias para configurar o
   // connection e outras coisas internas. Caso haja propriedades especificas
   // que precisam ser definidas, podem ser naturalmente colocados ai tambem
   // que o tipo de Connection específico poderar ler sem problema.
-  with FConfiguration do
+  with PersistenceService.Configuration do
   begin
     PropertyItem[cCONFIGKEY_DRIVER] := 'firebird-2.0';
     PropertyItem[cCONFIGKEY_USERNAME] := 'SYSDBA';
     PropertyItem[cCONFIGKEY_PASSWORD] := 'masterkey';
     PropertyItem[cCONFIGKEY_HOSTNAME] := 'localhost';
-    PropertyItem[cCONFIGKEY_DATABASENAME] := 
+    PropertyItem[cCONFIGKEY_DATABASENAME] :=
       ExtractFilePath(Application.ExeName) + 'Data\DBDEMOS.FDB';
     PropertyItem[cCONFIGKEY_TEMPLATETYPE] := 'TemplateReader_IO';
     PropertyItem[cCONFIGKEY_TEMPLATEPATH] :=
@@ -59,7 +61,6 @@ end;
 procedure TPersistenceTests.TearDown;
 begin
   inherited;
-  FConfiguration.Clear;
 end;
 
 procedure TPersistenceTests.TestLoadObjectByOID;
@@ -84,6 +85,17 @@ begin
   CheckEquals('1361-2', vObj.AccountNumber.AsString, 'Número da conta incompatível');
   CheckTrue(SameValue(125.3, vObj.InitialBalance.AsDouble), 'Saldo inicial incompatível');
   CheckTrue(SameValue(1524.25, vObj.CurrentBalance.AsDouble), 'Saldo atual incompatível');
+end;
+
+procedure TPersistenceTests.TestParse;
+var
+  vP: IParseParams;
+  sL: TStrings;
+begin
+  vP := TParseParams.Create('Select #MacroParam* ::Teste ##abc #:yyy #:xxx from teste where x = :teste1');
+  sL := vP.GetParams;
+  ShowMessage(Sl.Text);
+  // CheckEquals('teste1', sL[0], 'Não sao identicos');
 end;
 
 initialization
