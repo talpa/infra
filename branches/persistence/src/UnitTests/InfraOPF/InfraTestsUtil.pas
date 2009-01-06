362d8d79f1041b019ad649ee37c7be419a91d484
marcosg.duarte <marcosg.duarte@56d1e7d9-9621-0410-b556-b1a0954dd335>
2009-01-06 01:38:42 +0000

    CHG: Corrigido o nome da classe de testes de TConfiguration NEW: Classe TDBZeosExecutor para executar instruções SQL direto no banco

--- branches/persistence/src/UnitTests/InfraOPF/InfraTestsUtil.pas
+++ branches/persistence/src/UnitTests/InfraOPF/InfraTestsUtil.pas
@@ -3,9 +3,18 @@ unit InfraTestsUtil;
 interface
 
 uses
-  InfraPersistenceIntf;
+  InfraPersistenceIntf, ZDbcIntfs;
 
 type
+  TDBZeosExecutor = class
+  private
+    FConnection: IZConnection;
+  public
+    procedure OpenConnection(pConnString: string);
+    function Open(pSql: string): IZResultSet;
+    function Execute(pSql: string): Integer;
+  end;
+
   TTestsUtil = class
   public
     class function GetNewConfiguration: IConfiguration;
@@ -21,6 +30,30 @@ begin
   Result := TConfiguration.Create;
   Result.SetValue(cCONFIGKEY_CONNECTIONTIME, 10);
   Result.SetValue(cCONFIGKEY_MAXCONNECTIONS, 2);
+end;
+
+{ TDBZeosExecutor }
+
+procedure TDBZeosExecutor.OpenConnection(pConnString: string);
+begin
+  inherited Create;
+  FConnection := DriverManager.GetConnection(pConnString);
+end;
+
+function TDBZeosExecutor.Execute(pSql: string): Integer;
+var
+  vStatement: IZStatement;
+begin
+  vStatement := FConnection.CreateStatement;
+  Result := vStatement.ExecuteUpdate(pSql);
+end;
+
+function TDBZeosExecutor.Open(pSql: string): IZResultSet;
+var
+  vStatement: IZStatement;
+begin
+  vStatement := FConnection.CreateStatement;
+  Result := vStatement.ExecuteQuery(pSql);
 end;
 
 end.
